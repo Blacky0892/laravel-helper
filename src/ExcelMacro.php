8c3cded9d9dd7ba89e4aab3855559bc578525965
Anton Mironov <MironovAB@gppc.ru>
2021-12-27 12:18:21 +0300

    init

---
+++ src/ExcelMacro.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types = 1);
+
+namespace Blacky0892\LaravelHelper;
+
+use Maatwebsite\Excel\Sheet;
+use Maatwebsite\Excel\Writer;
+
+Sheet::macro(
+    'styleCells',
+    function (Sheet $sheet, string $cellRange, array $style) {
+        $sheet->getDelegate()->getStyle($cellRange)->applyFromArray($style);
+    }
+);
+
+Sheet::macro(
+    'wrapText',
+    function (Sheet $sheet, string $cellRange, ?bool $wrap = true) {
+        $sheet->getDelegate()->getStyle($cellRange)->getAlignment()->setWrapText($wrap);
+    }
+);
+
+Sheet::macro(
+    'setOrientation',
+    function (Sheet $sheet, $orientation) {
+        $sheet->getDelegate()->getPageSetup()->setOrientation($orientation);
+    }
+);
+
+Sheet::macro(
+    'setFillByColorInCell',
+    function (Sheet $sheet, $cell, string $cellRange) {
+        $sheet->styleCells(
+            $cellRange,
+            [
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'color'    => ['argb' => $sheet->getDelegate()->getCell($cell)],
+                ],
+            ]
+        );
+    }
+);
+
+Sheet::macro(
+    'hideColumn',
+    function (Sheet $sheet, string $column) {
+        $sheet->getDelegate()->getColumnDimension($column)->setVisible(false);
+    }
+);
+
+Sheet::macro(
+    'setWidth',
+    function (Sheet $sheet, $column, ?float $width = null) {
+        if(is_array($column)){
+            foreach ($column as $name => $width){
+                $sheet->getDelegate()->getColumnDimension($name)->setWidth($width);
+            }
+        }
+        else $sheet->getDelegate()->getColumnDimension($column)->setWidth($width);
+    }
+);
+
+Writer::macro(
+    'setDefaultProperties',
+    function (Writer $writer) {
+        $writer->getDelegate()->getProperties()
+            ->setCreator('ГППЦ')
+            ->setLastModifiedBy("ГППЦ")
+            ->setTitle("ГППЦ выгрузка")
+            ->setSubject("ГППЦ выгрузка")
+            ->setDescription(
+                "ГППЦ выгрузка"
+            );
+    }
+);
+
+Writer::macro(
+    'setCreator',
+    function (Writer $writer, string $creator) {
+        $writer->getDelegate()->getProperties()->setCreator($creator);
+    }
+);
+
+abstract class ExcelMacro
+{
+}
---
+++ src/LaravelHelperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Blacky0892\LaravelHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
